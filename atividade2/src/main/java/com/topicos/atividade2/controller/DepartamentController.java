cad0b2835d0671ae41db451738bb3e088dcd6eae
Emerson Gabriel <87777005+emersonfontes@users.noreply.github.com>
2023-12-15 22:00:55 -0300

    UPDATE

--- atividade2/src/main/java/com/topicos/atividade2/controller/DepartamentController.java
+++ atividade2/src/main/java/com/topicos/atividade2/controller/DepartamentController.java
@@ -2,12 +2,11 @@ package com.topicos.atividade2.controller;
 
 import com.topicos.atividade2.model.Departament;
 import com.topicos.atividade2.repository.DepartamentRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/departaments")
@@ -22,6 +21,43 @@ public class DepartamentController {
     public List<Departament> getAll(){
 
         return departamentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Departament> getById(@PathVariable Long id){
+
+        if(departamentRepository.existsById(id)){
+            return departamentRepository.findById(id);
+        }else{
+            return null;
+        }
+    }
+    @PostMapping
+    public Long save(@RequestBody Departament departament){
+
+        departamentRepository.save(departament);
+
+        return departament.getId();
+    }
+
+    @PutMapping("/{id}")
+    public Departament alter (@PathVariable Long id, @RequestBody  Departament departament){
+
+        if(departamentRepository.existsById(id)){
+            return departamentRepository.save(departament);
+        }else{
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete (@PathVariable Long id){
+        if(departamentRepository.existsById(id)){
+            departamentRepository.deleteById(id);
+            return "Sucesso";
+        }else{
+            return "Falha";
+        }
     }
 
 }
--- atividade2/src/main/java/com/topicos/atividade2/controller/UserController.java
+++ atividade2/src/main/java/com/topicos/atividade2/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.topicos.atividade2.controller;
 
+import com.topicos.atividade2.model.Departament;
 import com.topicos.atividade2.model.User;
 import com.topicos.atividade2.repository.UserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -23,9 +22,46 @@ public class UserController {
 
 
     @GetMapping
-    public List<User> getAll (){
+    public List<User> getAll(){
 
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getById(@PathVariable Long id){
+
+        if(userRepository.existsById(id)){
+            return userRepository.findById(id);
+        }else{
+            return null;
+        }
+    }
+    @PostMapping
+    public Long save(@RequestBody User user){
+
+        userRepository.save(user);
+
+        return user.getId();
+    }
+
+    @PutMapping("/{id}")
+    public User alter (@PathVariable Long id, @RequestBody  User user){
+
+        if(userRepository.existsById(id)){
+            return userRepository.save(user);
+        }else{
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete (@PathVariable Long id){
+        if(userRepository.existsById(id)){
+            userRepository.deleteById(id);
+            return "Sucesso";
+        }else{
+            return "Falha";
+        }
     }
 
 
